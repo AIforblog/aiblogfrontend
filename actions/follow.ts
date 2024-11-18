377918cc0ad000b3af7432783aedcbc874eb72a8
Adekola Toniloba <atoniloba@gmail.com>
2024-11-18 20:21:55 +0100

    fixing build errors

--- actions/follow.ts
+++ actions/follow.ts
@@ -6,33 +6,38 @@ import { authenticatedAction } from "@/lib/safe-action";
 import { z } from "zod";
 
 export const action = authenticatedAction
-	.createServerAction()
-	.input(
-		z.object({
-			followeeId: z.string().uuid(),
-			path: z.string(),
-		}),
-	)
-	.handler(async ({ input: { followeeId, path } }) => {
-		type Success = {
-			message: string;
-		};
-		const user = await assertUserAuthenticated();
-		const followUser = makeFetch<Success>(
-			"auth",
-			`/auth/${path}/${followeeId}`,
-			user.accessToken.value,
-			{
-				method: "POST",
-			},
-		);
-		try {
-			return await followUser();
-		} catch (err) {
-			console.log(err);
-		}
-		console.log(followeeId);
-	});
+
+  .createServerAction()
+  .input(
+    z.object({
+      followeeId: z.string().uuid(),
+      path: z.string(),
+    })
+  )
+  .handler(async ({ input: { followeeId, path } }) => {
+    type Success = {
+      message: string;
+    };
+
+    const user = await assertUserAuthenticated();
+    if (!user) {
+      throw new Error("User authentication failed");
+    }
+    const followUser = makeFetch<Success>(
+      "auth",
+      `/auth/${path}/${followeeId}`,
+      user.accessToken.value,
+      {
+        method: "POST",
+      }
+    );
+    try {
+      return await followUser();
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(followeeId);
+  });
 
 // export const unfollowAction = authenticatedAction
 // 	.createServerAction()
